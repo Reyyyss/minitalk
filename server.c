872d9375a10004b87be592382107949a2148015d
Reyyyss <hcr2207@gmail.com>
2025-04-24 15:35:42 +0100

    server

--- server.c
+++ server.c
@@ -6,26 +6,57 @@
 /*   By: hcarrasq <hcarrasq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 18:15:40 by hcarrasq          #+#    #+#             */
-/*   Updated: 2025/04/23 16:05:50 by hcarrasq         ###   ########.fr       */
+/*   Updated: 2025/04/24 15:34:09 by hcarrasq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void signal_handler(int sig, siginfo_t *info, void *ucontext)
+void message_receiver(int sig, int len)
 {
-	static int		byte_len;
-	static char		byte;
+	int	chars;
+	static int		bits;
+	static char		*str;
+	static char		c;
 
-	byte_len = 0;
-	if (sig == SIGUSR2)
-		byte |= 1 << byte_len;
-	byte_len++;
-	if (byte_len == 8)
+	if (bits < 1)
+		str = malloc(len);
+	if (sig == SIGUSR1)
+		c |= 1 << bits;
+	bits++;
+	if (bits % 8 == 0)
 	{
-		write(1, byte, 1);
-		byte_len = 0;
-		byte = 0;
+		if (chars < len)
+		{
+			str[chars] = c;
+			chars++;
+		}
+		else if (chars == len)
+			ft_printf("%s", str);
+		c = 0;
+	}
+}
+void signal_handler(int sig, siginfo_t *client, void *ucontext)
+{
+	static int		bit_len;
+	static int		bits;
+	static pid_t	client_id;
+
+	if (!client_id || client_id != client->si_pid)
+	{
+		client_id = client->si_pid;
+		bits = 0;
+	}
+	if (bits < 32)
+	{
+		bit_len = bit_len << 1;
+		if (sig == SIGUSR1)
+			bit_len |= 1;
+		bits++;
+	}
+	if (bits == 32)
+	{
+		message_receiver(sig, bit_len);
 	}
 }
 
